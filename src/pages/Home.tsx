74b1e4f9b3de319605fdfdc4d786e9d621e87a5f
The Real Tech Lady <bukolaarowosebi@gmail.com>
2022-03-07 15:09:07 +0100

    Slider example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,93 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSlides, IonSlide, IonButton, IonCard, IonCardContent, IonCardTitle, IonNote, IonLabel } from '@ionic/react';
 import './Home.css';
+// Import Swiper React components
+import { Swiper, SwiperSlide, useSwiper} from 'swiper/react';
 
-const Home: React.FC = () => {
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Navigation } from "swiper";
+
+
+const Home: React.FC = () => {  
+  const swiper = useSwiper();
+  //array for data to be used
+  const data =[
+    {
+      title: "Goals",
+      subTitle: "Setting Goals for Productivity",
+      image: "/assets/images/goals.jpg"
+    },
+    {
+      title: "Alarms",
+      subTitle: "Get up. Do it NOW!",
+      image:"/assets/images/alarms.jpg"
+    },
+    {
+      title: "Reminders",
+      subTitle: "Productivity at its peak.",
+      image: "/assets/images/reminders.jpg"
+    }
+  ]
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <IonContent scrollX>
+      <Swiper       
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: false }}
+        spaceBetween={100}
+        slidesPerView={1}
+        navigation={true} modules={[Navigation]}
+      >
+          {data.map((card, index) => {
+           return(
+             <SwiperSlide key={`slide_${index}`}>
+               <div className='mainContainer'>
+              
+                 <img src={card.image} alt="Goals" className='image' />
+
+                 <div className='innerContainer'>
+                   <IonLabel className='title'>{card.title}</IonLabel>
+                   <IonLabel className='subtitle'>{card.subTitle}</IonLabel> 
+                 </div>      
+              </div>
+             </SwiperSlide>
+           )
+         })}
+
+         <SwiperSlide>
+           <IonButton routerLink="/welcome">Get STARTED</IonButton>
+         </SwiperSlide>
+
+
+
+      </Swiper>
+      
+         {/* {data.map((card, index) => {
+           return(
+             <SwiperSlide key={`slide_${index}`}>
+               <IonCard>
+                 <img src={card.image} alt="Goals" className='image' />
+                 <IonCardContent>
+                   <IonCardTitle className='title'>{card.title}</IonCardTitle>
+                   <IonNote className='subtitle'>{card.subTitle}</IonNote>
+                 </IonCardContent>
+               </IonCard>
+             </SwiperSlide>
+           )
+         })}      */}
+
+      
+        
+      
+        
+      
+      
+    </IonContent>
   );
 };
 
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,17 @@
+import { IonContent, IonLabel, IonPage } from "@ionic/react"
+
+const Welcome: React.FC = () => {  
+    return(
+        <IonPage>
+            <IonContent>
+            <IonLabel>
+                Welcome Page
+            </IonLabel>
+
+            </IonContent>    
+
+        </IonPage>
+    )
+}
+
+export default Welcome;
